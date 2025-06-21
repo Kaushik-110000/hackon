40a68c9b57d061fd48ff0eeb3929365b55b4b6fe
Kaushik-110000 <abhishekkumar89647@gmail.com>
2025-06-21 18:05:37 +0530

    a bit change in recomm

--- src/app/api/products/searchtrie/route.ts
+++ src/app/api/products/searchtrie/route.ts
@@ -6,16 +6,18 @@ export async function GET(req: NextRequest) {
   try {
     await connectDB();
 
-    const products = await Product.find().lean();
+  
+    const result = await Product.aggregate([
+      {
+        $project: {
+          tokens: { $split: [{ $toLower: "$name" }, " "] }
+        }
+      },
+      { $unwind: "$tokens" },
+      { $group: { _id: "$tokens" } },
+    ]);
 
-    const rawTokens = products.flatMap((p) => {
-      const nameTokens = String(p.name).split(/\s+/);
-      const categoryTokens = String(p.category).split(/\s+/);
-      const brandTokens = String(p.brand).split(/\s+/);
-      return [...nameTokens, ...categoryTokens, ...brandTokens];
-    });
-
-    const tokens = Array.from(new Set(rawTokens.map((t) => t.toLowerCase())));
+    const tokens = result.map((r) => r._id);
 
     return NextResponse.json({ status: 200, tokens }, { status: 200 });
   } catch (error) {
