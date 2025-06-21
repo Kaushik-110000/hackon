3c60c7a63600ef634c84da02148faa6d328f25e0
Akash <akashjais9341@gmail.com>
2025-06-22 01:01:43 +0530

    bot working

--- src/app/green-store/page.tsx
+++ src/app/green-store/page.tsx
@@ -14,6 +14,7 @@ import type { FC } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import ChatBotWidget from "../../components/ChatBotWidget";
 
 interface Product {
   _id: string;
@@ -687,6 +688,8 @@ export default function GreenStore() {
           </div>
         </div>
       </div>
+       {/* Sprout */}
+        <ChatBotWidget />
     </div>
   );
 }
